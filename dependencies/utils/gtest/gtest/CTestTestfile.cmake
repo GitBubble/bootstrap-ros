e243eb944ff3f07025d0a96c7f3344c012c258a1
root <root@centos-linux.shared>
2018-11-25 19:52:55 +0800

    remove gtest

--- dependencies/utils/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/huawei/mecha/ros_varient/sim-install/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
