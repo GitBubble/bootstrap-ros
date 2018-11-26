47a4277f4f88cfef81fb336e4771e29d018ecc74
GitBubble <greatdwb@gmail.com>
2018-11-26 17:59:20 +0800

    fix build basic

--- script/basic_build.sh
+++ script/basic_build.sh
@@ -1,3 +1,12 @@
 #! /usr/bin/bash
 
+cd "$(dirname "$0")"
+
 ./autoconf_build.sh
+./automake_build.sh
+./libtool_build.sh
+./pkg_config_build.sh
+./cmake_build.sh
+# sbcl need zlib.sh
+./zlib_build.sh
+./sbcl_bootstrap.sh
