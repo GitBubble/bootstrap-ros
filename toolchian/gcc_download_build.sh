848b3941f61092488fc306c369ae296b8e3677d7
root <root@centos-linux.shared>
2018-12-02 23:52:08 +0800

    gcc ok

--- toolchian/gcc_download_build.sh
+++ toolchian/gcc_download_build.sh
@@ -6,42 +6,24 @@ GMP=gmp-4.3.2
 MPC=mpc-1.0.3
 
 
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/$MPFR.tar.bz2 || exit 1
-tar xjf $MPFR.tar.bz2 || exit 1
-ln -sf $MPFR mpfr || exit 1
+yum -y install compat-glibc glibc-devel
+yum -y install gcc gcc-c++
 
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/$GMP.tar.bz2 || exit 1
-tar xjf $GMP.tar.bz2  || exit 1
-ln -sf $GMP gmp || exit 1
-
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/$MPC.tar.gz || exit 1
-tar xzf $MPC.tar.gz || exit 1
-ln -sf $MPC mpc || exit 1
-
-rm $MPFR.tar.bz2 $GMP.tar.bz2 $MPC.tar.gz || exit 1
-
-#if [ ! -f ./mpfr-2.4.2.tar.gz ]; then 
-#  wget https://mirrors.ustc.edu.cn/gnu/mpfr/mpfr-2.4.2.tar.gz 
-#fi
-#
-#
-#
-#if [ ! -f ./mpc-1.1.0.tar.gz ]; then 
-#  wget https://mirrors.ustc.edu.cn/gnu/mpc/mpc-1.1.0.tar.gz 
-#fi
 #
 if [ ! -f ./gcc-4.8.0.tar.gz ]; then 
    wget https://mirrors.ustc.edu.cn/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.gz -O gcc-4.8.0.tar.gz
    tar xzvf gcc-4.8.0.tar.gz
+   cp ./download_prerequisites  ./gcc-4.8.0/contrib/download_prerequisites
+   cd ./gcc-4.8.0  
+   ./contrib/download_prerequisites
 fi
 
-
+cd ../
 mkdir objdir
 
 cd objdir
 
-../gcc-4.8.0/configure --prefix=/opt/gcc --disable-multilib --enable-languages=c,c++
-#../gcc-4.8.0/contrib/
+../gcc-4.8.0/configure --prefix=/opt --disable-multilib --enable-languages=c,c++
 make -j32
 make install
 
