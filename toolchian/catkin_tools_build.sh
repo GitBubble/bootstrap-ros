8a176d7061a55843c4e6802ae836f57efaea415e
root <root@localhost.localdomain>
2018-12-04 16:48:39 +0800

    python catkin tools source

---
+++ toolchian/catkin_tools_build.sh
@@ -0,0 +1,15 @@
+# /usr/bin/bash
+
+cd catkin_tools_all
+
+pip install -e alabaster-0.7.12    
+pip install -e snowballstemmer-1.2.1
+pip install -e futures-3.2.0        
+pip install -e sphinxcontrib-programoutput-0.11
+pip install -e Jinja2-2.10          
+pip install -e sphinx_rtd_theme-0.4.2
+pip install -e MarkupSafe-1.1.0     
+pip install -e Sphinx-1.3.6
+pip install -e trollius-trollius-2.2
+pip install -e osrf_pycommon-0.1.6
+pip install -e catkin_tools        
