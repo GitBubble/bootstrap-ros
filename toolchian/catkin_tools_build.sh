d2ccb0557c09296c2200fa0f2f80b1f55c6479da
文彬 邓 <test@test-centos.shared>
2018-12-08 17:22:25 +0800

    add comment in catkin tools build

--- toolchian/catkin_tools_build.sh
+++ toolchian/catkin_tools_build.sh
@@ -5,11 +5,13 @@ cd catkin_tools_all
 pip install -e alabaster-0.7.12    
 pip install -e snowballstemmer-1.2.1
 pip install -e futures-3.2.0        
-pip install -e sphinxcontrib-programoutput-0.11
 pip install -e Jinja2-2.10          
+# still missing the following package in following test
+#Jinja2-2.10 MarkupSafe-1.1.0 Pygments-2.3.0 Sphinx-1.8.2 babel-2.6.0 imagesize-1.1.0 packaging-18.0 pyparsing-2.3.0 sphinxcontrib-programoutput sphinxcontrib-websupport-1.1.0 typing-3.6.6
+pip install -e Sphinx-1.3.6
+pip install -e sphinxcontrib-programoutput-0.11
 pip install -e sphinx_rtd_theme-0.4.2
 pip install -e MarkupSafe-1.1.0     
-pip install -e Sphinx-1.3.6
 pip install -e trollius-trollius-2.2
 pip install -e osrf_pycommon-0.1.6
 pip install -e catkin_tools        
